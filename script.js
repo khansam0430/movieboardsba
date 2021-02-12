b99845bd7d1cfb3f946a55413906f84caa931fc9
Samreen Khan <khansam0430@gmail.com>
2021-02-12 14:07:54 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('connected!');
+
+let moviesArray = [];
+let userInput= " ";
+
+function addTo(){
+            
+        if (userInput === null){
+            alert("You must enter a valid response");
+           
+        } else{
+            document.getElementById("movieList").innerHTML=" ";
+            moviesArray.push(document.getElementById("userInput").value);
+            
+            for(i = 0; i < moviesArray.length; i++){
+                {document.getElementById("movieList").innerHTML += moviesArray[i]}
+             }
+        }
+    console.log(moviesArray);
+}
